7520aa67150892066a2cb807342dc0fb75e81a18
Vishak-P <147609054+Vishak-P@users.noreply.github.com>
2026-07-13 10:31:22 +0530

    Time: 5 ms (61.44%), Space: 44.3 MB (74.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int []fre=new int[26];
+        for(int i=0;i<s.length();i++){
+            fre[s.charAt(i)-'a']++;
+            fre[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<fre.length;i++){
+            if(fre[i]!=0) return false;
+        }
+        return true;
+    }
+}
